c518ba4d4b9da941191744591a59d446a71c299b
Ahmed Samir <speechless_man@hotmail.com>
2024-09-08 19:14:08 +0300

    Update login.php

--- fb-login/login.php
+++ fb-login/login.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-	<head>
-		<meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,minimum-scale=1">
 		<title>Login with Facebook</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
